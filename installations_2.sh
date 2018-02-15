97e92250a78055ec31d9c379ab9dd2a50c429e87
Sudhishna <ssendhil@juniper.net>
2018-02-15 12:44:59 -0800

    Update installations_2.sh

--- installations_2.sh
+++ installations_2.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Install Ansible
+sudo pip install gitpython
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
 sudo apt-get install ansible -y
@@ -9,3 +10,5 @@ sudo apt-get install ansible -y
 ansible-galaxy install Juniper.junos,1.4.3
 
 sudo rm -rf EVPN-VXLAN/
+
+mkdir /home/lab/.ssh
