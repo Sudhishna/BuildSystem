15d608d35b714641efd1785caaf123d349d4d2f4
Sudhishna <ssendhil@juniper.net>
2018-04-01 18:14:42 -0700

    Update setup-config.py

--- setup-config.py
+++ setup-config.py
@@ -117,7 +117,9 @@ def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
+        sys.stdout.write("\r")
+        sys.stdout.write(timeformat) 
+        sys.stdout.flush()
         time.sleep(1)
         t -= 1
 
