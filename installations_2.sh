b6cf894a515125714ad6bcb449f2819087cd1c70
Sudhishna <ssendhil@juniper.net>
2018-04-01 18:27:56 -0700

    Update installations_2.sh

--- installations_2.sh
+++ installations_2.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-# Install Ansible
+# Install PIP packages
+sudo -H pip install --upgrade pip
+sudo -H pip install setuptools --upgrade
+sudo -H pip install pyopenssl ndg-httpsclient pyasn1
+sudo -H pip install ansible
+sudo -H pip install junos-eznc
+sudo -H pip install jxmlease
 sudo -H pip install gitpython
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
-sudo apt-get install ansible -y
+
+# Install PIP3 packages
+sudo apt-get install python3-pip -y
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install junos-eznc
+sudo -H pip3 install jxmlease
+sudo -H pip3 install gitpython
 
 # Install Ansible modules
-ansible-galaxy install Juniper.junos,1.4.3
+sudo ansible-galaxy install Juniper.junos
 
 rm -rf ~/EVPN-VXLAN
