abbe3616860040747333a38b6a27f5de655b1cb4
Sudhishna <ssendhil@juniper.net>
2018-04-01 18:26:35 -0700

    Update installations_1.sh

--- installations_1.sh
+++ installations_1.sh
@@ -3,17 +3,5 @@
 # To get the latest package lists 
 sudo apt-get update -y
 
-# Install Python
-sudo apt-get install python-pip python-dev libxml2-dev libxslt-dev libssl-dev libffi-dev -y
-sudo apt-get install libpython3-dev libpython3.5-dev python3-dev python3-pip python3-setuptools python3-wheel python3.5-dev -y
-
-# Upgrade pip
-sudo -H pip install --upgrade pip
-sudo -H pip3 install --upgrade pip
-
-# Install Juniper Libraries
-sudo -H pip2 install junos-eznc
-sudo -H pip2 install jxmlease
-sudo -H pip3 install junos-eznc
-sudo -H pip3 install jxmlease
-
+# Install Requred Python Packages
+sudo apt-get install python-minimal software-properties-common python python-pip sshpass apt-transport-https ca-certificates python-dev libffi-dev libssl-dev -y
