a47d7f1b5ac3d30651f8f5ac520c18533703b10f
Sudhishna <ssendhil@juniper.net>
2018-04-01 18:54:26 -0700

    Update installations_2.sh

--- installations_2.sh
+++ installations_2.sh
@@ -9,14 +9,14 @@ sudo -H pip install junos-eznc
 sudo -H pip install jxmlease
 sudo -H pip install gitpython
 
+# Install Ansible modules
+sudo ansible-galaxy install Juniper.junos
+
 # Install PIP3 packages
 sudo apt-get install python3-pip -y
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install junos-eznc
 sudo -H pip3 install jxmlease
 sudo -H pip3 install gitpython
-
-# Install Ansible modules
-sudo ansible-galaxy install Juniper.junos
 
 rm -rf ~/EVPN-VXLAN
