7ea873ba7872b5c652c4f8930648a54fc782f002
Sudhishna <ssendhil@juniper.net>
2018-08-22 10:24:43 -0700

    Update installations_1.sh

--- installations_1.sh
+++ installations_1.sh
@@ -2,6 +2,7 @@
 
 # To get the latest package lists 
 sudo apt-get update -y
+sudo apt-get upgrade
 
 # Install Requred Python Packages
 sudo apt-get install python-minimal software-properties-common python python-pip sshpass apt-transport-https ca-certificates python-dev libffi-dev libssl-dev -y
